cbb94489049f7d59f8c2fde2cc67acd6dc88a74b
vapeman <kiv@vivaldi.net>
2021-11-26 07:58:59 +0300

    Create a base layout for the currency-view

--- src/app/components/currency-view/currency-view.component.ts
+++ src/app/components/currency-view/currency-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-currency-view',
@@ -9,7 +9,21 @@ export class CurrencyViewComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  @Input() currencyCharCode: string = 'EUR'
+
+  public updating: boolean = false
+  public error: boolean = false
+  public currencyName: string = 'Евро'
+  public currencyValue: number = 83.6793
+  public currencyPreviousValue: number | null = null
+  public currencyValueIsGrowing: boolean | null | undefined = undefined; /* true -- growing, false -- falling,
+                                                                            null -- same, undefined -- unknown */
+  public currencyDateTime: string = ''
+  public message: string = 'Обновление курса...'
+
+  private data: any = undefined
+
+
+  ngOnInit(): void {}
 
 }
