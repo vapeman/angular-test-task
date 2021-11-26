842cd078ff95df39bcb91547bd7b46675d8e37e9
vapeman <kiv@vivaldi.net>
2021-11-26 05:45:42 +0300

    Add fade-in animation for the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { animate, style, transition, trigger } from "@angular/animations";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
+  animations: [
+    trigger('fadeInOutAnimation', [
+      // state('transparent', style({opacity: '0.0',})),
+      // state('notTransparent', style({opacity: '1.0',})),
+      // transition('transparent <=> notTransparent', animate('0.9s ease')),
+      transition('void => *', [
+        style({opacity: 0, filter: 'blur(5px)'}),
+        animate('.7s', style({opacity: 1, filter: 'none'}))
+      ]),
+      transition('* => void', [
+        animate('.7s', style({opacity: 0, filter: 'blur(5px)'}))
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
   title = 'angular-test-task';
