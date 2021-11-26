e13b23df8b51cf340c515217349b9bfe617fd15d
vapeman <kiv@vivaldi.net>
2021-11-26 14:41:58 +0300

    Add simple logic to the currency service (without polling)

--- src/app/services/currency/currency.service.ts
+++ src/app/services/currency/currency.service.ts
@@ -1,9 +1,56 @@
 import { Injectable } from '@angular/core';
 
+import { Observable, onErrorResumeNext, of } from "rxjs";
+import { first, catchError } from "rxjs/operators";
+
+import { CbrJsonService } from "./data-providers/cbr-json.service";
+import { CbrXmlService } from "./data-providers/cbr-xml.service";
+
+
+export interface Currency {
+  [currencyCode: string]: {
+    charCode: string
+    name: string
+    nominal: number
+    value: number
+    previousValue: number | null
+  }
+}
+export interface Currencies {
+  sourceUrl: string
+  timestamp: number
+  previousTimestamp: number | null
+  base: string
+  quotes: Currency
+}
+
+type CurrencyGetter = () => Observable<Currencies>;
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class CurrencyService {
 
-  constructor() { }
+  constructor(
+    private cbrJsonService: CbrJsonService,
+    private cbrXmlService: CbrXmlService,
+  ) { }
+
+  private readonly getters: CurrencyGetter[] = [
+    this.cbrXmlService.getData.bind(this.cbrXmlService),
+    this.cbrJsonService.getData.bind(this.cbrJsonService),
+  ]
+
+  public getData(): Observable<Currencies | null> {
+    // this.cbrXmlService.getData().subscribe(result => {console.log(result)});
+    return onErrorResumeNext(...this.getters.map(getter => getter())).pipe(
+      first(),
+      catchError(err => {
+        console.error(err);
+        return of(null);
+      })
+    );
+  }
+
 }
